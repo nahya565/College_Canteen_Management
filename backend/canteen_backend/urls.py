75da46adff1dbc3dfea368b0758664d5b53d1fbb
Nahya Jaswitha <nahyajaswitha@gmail.com>
2026-07-21 21:34:38 +0530

    Remove React routing for backend deployment

--- backend/canteen_backend/urls.py
+++ backend/canteen_backend/urls.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
-from django.views.static import serve
-from django.conf import settings
+from django.urls import path, include
+from django.http import HttpResponse
 
-DIST_DIR = settings.BASE_DIR.parent / 'dist'
+def home(request):
+    return HttpResponse("College Canteen Backend is Running Successfully 🚀")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    re_path(r'^assets/(?P<path>.*)$', serve, {'document_root': DIST_DIR / 'assets'}),
-    re_path(r'^images/(?P<path>.*)$', serve, {'document_root': DIST_DIR / 'images'}),
-    re_path(r'^(?P<path>favicon\.svg|icons\.svg)$', serve, {'document_root': DIST_DIR}),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='frontend'),
+    path("", home),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
 ]
-
-
